a72af9f6039560f268ffb700f1c3461d5ec3a30c
uto-usui <me@uto-usui.com>
2019-05-25 19:17:06 +0900

    ✨ Add scrollManager page

---
+++ pages/scrollManager/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrap js_parallaxWrap">
+    <DummyItems />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import DummyItems from '~/components/DummyItems.vue'
+import ScrollManager from '~/assets/js/scrollManager/scrollManager'
+
+interface data {
+  scrollManager: any
+}
+
+export default Vue.extend({
+  components: {
+    DummyItems,
+  },
+  data(): data {
+    return {
+      //
+      scrollManager: null,
+    }
+  },
+  mounted() {
+    this.scrollManager = new ScrollManager(this.$ua)
+    this.$nextTick(() => {
+      this.scrollManager.scrollEventAttach()
+      this.scrollManager.parallaxEventAttach()
+    })
+  },
+  updated() {
+    this.$nextTick(() => {
+      this.scrollManager.parallaxEventDetach()
+      this.scrollManager.parallaxEventAttach()
+    })
+  },
+  beforeDestroy() {
+    this.scrollManager.parallaxEventDetach()
+    this.scrollManager.scrollEventDetach()
+  },
+})
+</script>
+
+<style>
+.wrap {
+  width: 100%;
+  overflow: hidden;
+}
+</style>
