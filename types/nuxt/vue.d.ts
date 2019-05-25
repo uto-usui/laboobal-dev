8f8e42a1ae2caa40a2ec9140bd9fd7269d582a17
uto-usui <me@uto-usui.com>
2019-05-25 19:19:48 +0900

    🏷️ Add ua types

--- types/nuxt/vue.d.ts
+++ types/nuxt/vue.d.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import * as Vuex from 'vuex'
 
+import { UA } from 'nuxt-user-agent/lib/types'
+
 declare module 'vue/types/vue' {
   interface Vue {
     $exStore: Vuex.ExStore
@@ -8,5 +10,13 @@ declare module 'vue/types/vue' {
     $getters: Vuex.ExStore['getters']
     $commit: Vuex.ExStore['commit']
     $dispatch: Vuex.ExStore['dispatch']
+    //
+    $ua: UA
+  }
+}
+
+declare module '@nuxt/vue-app/types/index' {
+  interface Context {
+    $ua: UA
   }
 }
