691223611648fd5bd5282f15fda20e584b9951c1
uto-usui <me@uto-usui.com>
2019-05-25 19:22:22 +0900

    ✨ Add contentsList

---
+++ components/ContentsList.vue
@@ -0,0 +1,44 @@
+<template>
+  <ul class="contents-list">
+    <li ref="target" class="cl__item">
+      <n-link class="cl__target" to="/scrollManager">scrollManager</n-link>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { TweenMax, Power0 } from 'gsap/umd/TweenMax'
+
+export default Vue.extend({
+  name: 'ContentsList',
+  mounted() {
+    this.$nextTick(() => {
+      this.animation()
+    })
+  },
+  methods: {
+    animation(): void {
+      TweenMax.to(this.$refs.target, 5, {
+        repeat: -1,
+        rotationY: 360,
+        ease: Power0.easeIn,
+      })
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.contents-list {
+  //
+}
+
+.cl__item {
+  //
+}
+
+.cl__target {
+  //
+}
+</style>
