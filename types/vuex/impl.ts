45da57a373a00533d495db593885855c44ad9030
uto-usui <me@uto-usui.com>
2019-08-23 10:08:07 +0900

    ✨ Add global store

--- types/vuex/impl.ts
+++ types/vuex/impl.ts
@@ -1,11 +1,12 @@
 import * as Sample from '../../store/sample/type'
+import * as Global from '../../store/global/type'
 
 declare module 'vuex' {
   type RootState = {
     sample: Sample.S
+    global: Global.S
   }
-  // type RootGetters = Counter.RG & Sample.RG
-  type RootGetters = Sample.RG
-  type RootMutations = Sample.RM
-  type RootActions = Sample.RA
+  type RootGetters = Sample.RG & Global.RG
+  type RootMutations = Sample.RM & Global.RM
+  type RootActions = Sample.RA & Global.RA
 }
