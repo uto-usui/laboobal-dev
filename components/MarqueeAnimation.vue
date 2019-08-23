ba251f4a5074d3bc083fff151526ed3c6cb2a6cd
uto-usui <me@uto-usui.com>
2019-08-23 20:00:28 +0900

    ✨ Update marquee

---
+++ components/MarqueeAnimation.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="marquee-animation">
+    <div class="ma__inner">
+      <div class="ma__item">
+        <span v-for="index in 4" :key="index" class="ma__text"
+          >marquee infinite animation</span
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'MarqueeAnimation',
+}
+</script>
+
+<style lang="scss" scoped>
+.marquee-animation {
+  position: absolute;
+  top: 50%;
+  width: 100vw;
+  height: 1em;
+  margin-top: -0.5em;
+  font-size: 10vw;
+  letter-spacing: 0.1em;
+  //
+  &--tb {
+    transform: translateX(0.5em) rotate(-90deg);
+  }
+  //
+  &--bt {
+    transform: translateX(-0.5em) rotate(90deg);
+  }
+  //
+  &--diag {
+    transform: translateX(2em) rotate(210deg);
+  }
+}
+
+.ma__inner {
+  position: relative;
+  z-index: 0;
+  width: 100%;
+}
+
+.ma__item {
+  position: absolute;
+  top: 0;
+  left: 50%;
+  z-index: 9;
+  font-size: 0;
+  white-space: nowrap;
+  transform: translate3d(-50%, 0, 0);
+  will-change: transfomrm;
+  backface-visibility: hidden;
+  animation: marquee 15s linear infinite;
+}
+
+.ma__text {
+  padding-right: 0.5em;
+  font-size: 10vw;
+  font-weight: bold;
+  line-height: 1;
+  color: rgba($color-white, 0.15);
+  text-transform: uppercase;
+}
+
+@keyframes marquee {
+  from {
+    transform: translate3d(-50%, 0, 0);
+  }
+  //
+  to {
+    transform: translate3d(-75%, 0, 0);
+  }
+}
+</style>
--- pages/marquee/index.vue
+++ pages/marquee/index.vue
@@ -1,19 +1,15 @@
 <template>
   <div class="wrap">
-    <div class="marquee-animation">
-      <div class="ma__inner">
-        <div class="ma__item">
-          <span v-for="index in 4" :key="index" class="ma__text">
-            marquee infinite animation
-          </span>
-        </div>
-      </div>
-    </div>
+    <marqueeAnimation />
+    <marqueeAnimation class="marquee-animation--tb" />
+    <marqueeAnimation class="marquee-animation--bt" />
+    <marqueeAnimation class="marquee-animation--diag" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import MarqueeAnimation from '@/components/MarqueeAnimation.vue'
 
 interface Data {
   //
@@ -21,7 +17,7 @@ interface Data {
 
 export default Vue.extend({
   components: {
-    // DummyItems,
+    MarqueeAnimation,
   },
   data(): Data {
     return {
@@ -51,51 +47,5 @@ export default Vue.extend({
   width: 100%;
   min-height: 100vh;
   overflow: hidden;
-}
-
-.marquee-animation {
-  position: absolute;
-  top: 50%;
-  width: 100vw;
-  margin-top: -0.5em;
-  font-size: 10vw;
-}
-
-.ma__inner {
-  position: relative;
-  z-index: 0;
-  width: 100%;
-}
-
-.ma__item {
-  position: absolute;
-  top: 0;
-  left: 50%;
-  z-index: 9;
-  font-size: 0;
-  white-space: nowrap;
-  transform: translate3d(-50%, 0, 0);
-  will-change: transfomr;
-  backface-visibility: hidden;
-  animation: marquee 15s linear infinite;
-}
-
-.ma__text {
-  padding-right: 0.5em;
-  font-size: 10vw;
-  font-weight: bold;
-  line-height: 1;
-  color: rgba($color-white, 0.15);
-  text-transform: uppercase;
-}
-
-@keyframes marquee {
-  from {
-    transform: translate3d(-50%, 0, 0);
-  }
-  //
-  to {
-    transform: translate3d(-75%, 0, 0);
-  }
 }
 </style>
