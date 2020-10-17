eba259f94f89cb64754af5dc9ccf4ea41ca7ebab
uto-usui <nattyanoranged@gmail.com>
2020-10-17 21:23:41 +0900

    🚨 Removing linter warnings.

--- plugins/splitting.client.ts
+++ plugins/splitting.client.ts
@@ -3,6 +3,6 @@ import 'splitting/dist/splitting.css'
 import 'splitting/dist/splitting-cells.css'
 
 export default (context, inject) => {
-  context.$splitting = options => Splitting.html(options)
-  inject('splitting', options => Splitting.html(options))
+  context.$splitting = (options) => Splitting.html(options)
+  inject('splitting', (options) => Splitting.html(options))
 }
